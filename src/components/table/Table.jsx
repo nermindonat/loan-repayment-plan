d7842391c72635853c42b309abd57ed1d599a2ae
Nermin Donat <70170606+nermindonat@users.noreply.github.com>
2022-09-15 00:03:02 +0300

    useRef Hook Added.

--- src/components/table/Table.jsx
+++ src/components/table/Table.jsx
@@ -2,21 +2,70 @@ import React from 'react'
 import "./table.css"
 
 const Table = () => {
+    
   return (
     <div className='container'>
-      <table>
-        <thead>
-            <tr>
-                <th>Taksit No</th>
-                <th>Taksit Tutarı</th>
-                <th>Ana Para</th>
-                <th>Kalan Ana Para</th>
-                <th>Kar Tutarı</th>
-                <th>KKDF</th>
-                <th>BSMV</th>
-            </tr>
-        </thead>
-      </table>
+        <span>Geri Ödeme Planı Tablosu</span>
+        <table>
+            <thead>
+                <tr>
+                    <th>Taksit No</th>
+                    <th>Taksit Tutarı</th>
+                    <th>Ana Para</th>
+                    <th>Kalan Ana Para</th>
+                    <th>Kar Tutarı</th>
+                    <th>KKDF</th>
+                    <th>BSMV</th>
+                </tr>
+            </thead>
+            <tbody>
+                <tr>
+                    <td>1</td>
+                    <td>acA</td>
+                    <td>XSC</td>
+                    <td>cczsc</td>
+                    <td>cssac</td>
+                    <td>csas</td>
+                    <td>sccsa</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>acA</td>
+                    <td>XSC</td>
+                    <td>cczsc</td>
+                    <td>cssac</td>
+                    <td>csas</td>
+                    <td>sccsa</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>acA</td>
+                    <td>XSC</td>
+                    <td>cczsc</td>
+                    <td>cssac</td>
+                    <td>csas</td>
+                    <td>sccsa</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>acA</td>
+                    <td>XSC</td>
+                    <td>cczsc</td>
+                    <td>cssac</td>
+                    <td>csas</td>
+                    <td>sccsa</td>
+                </tr>
+                <tr>
+                    <td>1</td>
+                    <td>acA</td>
+                    <td>XSC</td>
+                    <td>cczsc</td>
+                    <td>cssac</td>
+                    <td>csas</td>
+                    <td>sccsa</td>
+                </tr>
+            </tbody>
+        </table>
     </div>
   )
 }
--- src/components/userForm/UserForm.jsx
+++ src/components/userForm/UserForm.jsx
@@ -1,24 +1,53 @@
-import React from "react";
+import React, {useRef} from "react";
+import { useEffect } from "react";
 import "./userForm.css";
 
+
 const UserForm = () => {
+  const loanAmountRef = useRef();
+  const paymentNumberRef = useRef();
+  const profitRateRef = useRef();
+  const selectOneRef = useRef();
+  const selectTwoRef = useRef();
+  const selectThreeRef = useRef();
+
+
+  const handleChange = () => {
+    console.log(loanAmountRef.current.value, paymentNumberRef.current.value, profitRateRef.current.value, 
+      selectOneRef.current.value, selectTwoRef.current.value, selectThreeRef.current.value);
+  }
+
+  useEffect(() => {
+    console.log(loanAmountRef.current);
+  })
+
+
   return (
     <div className="user-form">
       <form>
         <label>Kredi Tutarı</label>
-        <input className="user-input" type="text" name="name" />
+        <input className="user-input" type="number" name="name" ref={loanAmountRef}/>
         <label>Taksit Sayısı</label>
-        <input className="user-input" type="text" name="name" />
+        <input className="user-input" type="number" name="name" ref={paymentNumberRef}/>
         <label>Kar Oranı</label>
-        <input className="user-input" type="text" name="name" />
+        <input className="user-input" type="number" name="name" ref={profitRateRef}/>
         <label>Taksit Aralığı</label>
-        <select name="select" id=""> 
+        <select name="select" id="" ref={selectOneRef}> 
           <option value="1">Haftalık</option>
           <option value="2">Aylık</option>
           <option value="3">Yıllık</option>
         </select>
         <label>Vergi Oranı</label>
-        <input className="user-input" type="text" name="name" />
+        <select name="select" id="" ref={selectTwoRef}> 
+          <option value="1">KKDF</option>
+          <option value="2">BSMV</option>
+        </select>
+        <label>Kar Formülü</label>
+        <select name="select" id="" ref={selectThreeRef}> 
+          <option value="1">Basit</option>
+          <option value="2">Bileşik</option>
+        </select>
+        <button onClick={handleChange}>Hesapla</button>
       </form>
     </div>
   );
