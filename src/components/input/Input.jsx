64ca2ff95af80fbe21e95f6c033f198feb25eff8
Nermin Donat <70170606+nermindonat@users.noreply.github.com>
2022-09-15 12:11:25 +0300

    useRef, useImpretiveHandle, forwardRef used.

---
+++ src/components/input/Input.jsx
@@ -0,0 +1,29 @@
+import React, { useRef, forwardRef, useImperativeHandle } from "react";
+import "./input.css";
+
+const Input = forwardRef((props, ref) => {
+  const { label, type = "text", name } = props;
+  const inputRef = useRef();
+  useImperativeHandle(ref, () => {
+    return {
+      getValue: () => {
+        return inputRef.current.value;
+      },
+    };
+  });
+
+  return (
+    <div>
+      <label>{label}</label>
+      <input
+        className="user-input"
+        type={type}
+        name={name}
+        ref={inputRef}
+        autoComplete="off"
+      />
+    </div>
+  );
+});
+
+export default Input;
--- src/components/userForm/UserForm.jsx
+++ src/components/userForm/UserForm.jsx
@@ -1,7 +1,6 @@
-import React, {useRef} from "react";
-import { useEffect } from "react";
+import Input from "../input/Input";
+import React, { useRef } from "react";
 import "./userForm.css";
-
 
 const UserForm = () => {
   const loanAmountRef = useRef();
@@ -11,43 +10,51 @@ const UserForm = () => {
   const selectTwoRef = useRef();
   const selectThreeRef = useRef();
 
-
-  const handleChange = () => {
-    console.log(loanAmountRef.current.value, paymentNumberRef.current.value, profitRateRef.current.value, 
-      selectOneRef.current.value, selectTwoRef.current.value, selectThreeRef.current.value);
-  }
-
-  useEffect(() => {
-    console.log(loanAmountRef.current);
-  })
-
+  const formSubmitHandler = (event) => {
+    event.preventDefault();
+    console.log(loanAmountRef.current.getValue(), paymentNumberRef.current.getValue(), profitRateRef.current.getValue());
+  };
 
   return (
     <div className="user-form">
       <form>
-        <label>Kredi Tutarı</label>
-        <input className="user-input" type="number" name="name" ref={loanAmountRef}/>
-        <label>Taksit Sayısı</label>
-        <input className="user-input" type="number" name="name" ref={paymentNumberRef}/>
-        <label>Kar Oranı</label>
-        <input className="user-input" type="number" name="name" ref={profitRateRef}/>
+        <Input
+          label={"Kredi Tutari"}
+          type="number"
+          name={"krediTutari"}
+          ref={loanAmountRef}
+        />
+        <Input
+          label={"Taksit sayisi"}
+          type="number"
+          name={"taksitSayisi"}
+          ref={paymentNumberRef}
+        />
+        <Input
+          label={"Kar orani"}
+          type="number"
+          name={"karOrani"}
+          ref={profitRateRef}
+        />
         <label>Taksit Aralığı</label>
-        <select name="select" id="" ref={selectOneRef}> 
+        <select name="select" id="" ref={selectOneRef}>
           <option value="1">Haftalık</option>
           <option value="2">Aylık</option>
           <option value="3">Yıllık</option>
         </select>
         <label>Vergi Oranı</label>
-        <select name="select" id="" ref={selectTwoRef}> 
+        <select name="select" id="" ref={selectTwoRef}>
           <option value="1">KKDF</option>
           <option value="2">BSMV</option>
         </select>
         <label>Kar Formülü</label>
-        <select name="select" id="" ref={selectThreeRef}> 
+        <select name="select" id="" ref={selectThreeRef}>
           <option value="1">Basit</option>
           <option value="2">Bileşik</option>
         </select>
-        <button onClick={handleChange}>Hesapla</button>
+        <button type="submit" onClick={formSubmitHandler}>
+          Submit
+        </button>
       </form>
     </div>
   );
